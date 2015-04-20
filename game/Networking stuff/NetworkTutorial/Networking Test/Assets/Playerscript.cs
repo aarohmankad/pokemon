53d05a5080e0153a2b8991e1c54e129f9bf9a5b3
startwarfields <jigglymex@gmail.com>
2015-04-19 18:17:47 -0700

    networking tutorial
    FSDFSF

---
+++ game/Networking stuff/NetworkTutorial/Networking Test/Assets/Playerscript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playerscript : MonoBehaviour {
+	public float speed = 10f;
+
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+
+
+	void Update()
+	{
+		if (networkView.isMine) 
+		{
+			InputMovement();
+		}
+		else
+		{
+			SyncedMovement();
+		}
+
+	}
+	
+	void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.S))
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D))
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.A))
+			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+	}
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero; //We will be adding velocity into the consideration 
+		if (stream.isWriting)
+		{
+			syncPosition = rigidbody.position;
+			stream.Serialize(ref syncPosition);
+			
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncVelocity);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay; //We multiply the velocity by the delay to create 
+																		//the effect of movement prediction through times of lag.
+			syncStartPosition = rigidbody.position;
+		}
+	}
+}
---
+++ game/Networking stuff/NetworkTutorial/Scripts/IPshower.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+#pragma implicit
+#pragma downcast
+import System.Net;
+
+function OnGUI () {
+	
+	var strHostName:String = "";
+strHostName = System.Net.Dns.GetHostName();
+var ipEntry:IPHostEntry = System.Net.Dns.GetHostEntry(strHostName);
+var addr:IPAddress[] = ipEntry.AddressList;
+	
+	
+	GUILayout.BeginArea(Rect(Screen.width/2+100,-250,400,Screen.height));
+	
+	GUILayout.FlexibleSpace();	
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();	
+	GUILayout.Label("Your IP:" +addr[addr.Length-1].ToString());
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	GUILayout.Space(10);
+	GUILayout.BeginHorizontal();
+	GUILayout.BeginVertical();
+	GUILayout.EndVertical();
+	GUILayout.EndHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.EndArea();
+	
+}
