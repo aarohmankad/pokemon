f0587ac730458970f6a9d0f17c46c226df1377fc
Aaroh Mankad <aarohmankad@gmail.com>
2015-04-24 13:14:58 -0700

    began to animate ash

--- game/Assets/Scripts/MovementJS.js
+++ game/Assets/Scripts/MovementJS.js
@@ -5,7 +5,11 @@
 	
 	var cc : CharacterController;
 	var anim : Animator;
-
+	
+	var speedModify : int = 2;
+	
+	var walkNum : int = 0;
+	var randomIntTime : boolean = false;
 function Start () {
 
 	cc = GetComponent(CharacterController);
@@ -15,19 +19,39 @@ function Start () {
 
 function Update () {
 
-	direction = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	direction = transform.rotation * new Vector3(Input.GetAxis("Horizontal")/speedModify, 0, Input.GetAxis("Vertical")/speedModify);
 
 	if(direction.magnitude > 1f)
 	{
 		direction = direction.normalized;
 	}
+	
+	if(Input.GetKey(KeyCode.LeftShift))
+	{
+		speedModify = 1;
+		anim.SetBool("Run",true);
+	}
+	else
+	{
+		speedModify = 2;
+		anim.SetBool("Run",false);
+	}
+	
+	if(Input.GetKeyDown(KeyCode.W))
+	{
+		randomIntTime = true;
+	}
+	else
+	{
+		anim.SetBool("Walk01",false);
+		anim.SetBool("Walk",false);
+		anim.SetBool("Walk02",false);
+	}
+	
+	if(randomIntTime)
+		//RandomWalk ();
 
 	anim.SetFloat ("Speed", direction.magnitude);
-	
-	if(anim.GetCurrentAnimatorStateInfo(0).IsName("JogForward_NtrlFaceFwd"))
-	{
-		
-	}
 
 }
 
@@ -36,6 +60,30 @@ function Update () {
 function FixedUpdate () {
 
 		cc.SimpleMove (direction * speed);
-		Debug.Log (direction);
+		//Debug.Log (direction);
+
+}
+
+function RandomWalk () {
+
+	walkNum = Random.Range(0,3);
+	print("walk num = " + walkNum);
+	
+	if(walkNum == 0)
+	{
+		anim.SetBool("Walk01",true);
+		print("w1 true");
+	}
+	else if(walkNum == 1)
+	{
+		anim.SetBool("Walk",true);
+		print("w true");
+	}
+	else
+	{
+		anim.SetBool("Walk02",true);
+		print("w2 true");
+	}
+	randomIntTime = false;
 
 }
